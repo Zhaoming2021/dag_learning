5c12fa83b2ae2db6e615734e6d01ec08e3d3ec10
Zhaoming2021 <91507587+Zhaoming2021@users.noreply.github.com>
2023-03-05 12:28:39 -0600

    Update dag_function.py

--- dag_function.py
+++ dag_function.py
@@ -9,13 +9,14 @@ import scipy.linalg as sla
 import numpy.linalg as la
 from trace_expm import trace_expm
 
-class notears:
-    def __init__(self,model, X, W, dims,s=1.0):
+class dag_functions:
+    def __init__(self, model, X, W, dims, s=1.0):
         self.model = model
         self.X = X
         self.W = W
-        self.dims = dims
+        self.dims, self.d = dims, dims[0]
         self.s = s
+        self.I = torch.eye(self.d)
 
     def h(self):
         if self.model.type == "mlp_signed":
@@ -28,15 +29,17 @@ class notears:
 
         elif self.model.type == "linear_signed":
             # similar to h in dagma linear
-            M = self.s * self.Id - self.W * self.W
-            h = - la.slogdet(M)[1] + self.d * np.log(self.s)
-            G_h = 2 * self.W * sla.inv(M).T 
+            M = self.s * self.Id - self.W @ self.W
+            h = - torch.slogdet(M)[1] + self.dims[0] * np.log(self.s)
+            G_h = 2 * self.W @ torch.inverse(M).T 
             return h, G_h
 
         elif self.model.type == "mlp_unsigned":
             #similar to h in notears nonlinear
             d = self.dims[0]
-            fc1_weight = self.fc1_pos.weight - self.fc1_neg.weight
+            fc1_pos_weight = self.model.fc1_pos.weight
+            fc1_neg_weight = self.model.fc1_neg.weight
+            fc1_weight = fc1_pos_weight - fc1_neg_weight
             fc1_weight = fc1_weight.view(d, -1, d)
             A = torch.sum(fc1_weight * fc1_weight, dim=1).t()
             h = trace_expm(A) - d
@@ -46,11 +49,7 @@ class notears:
             
             """Evaluate value and gradient of acyclicity constraint."""
             n, d = self.X.shape
-            E = slin.expm(self.W * self.W)  # (Zheng et al. 2018)
-            h = np.trace(E) - d
-            #     # A different formulation, slightly faster at the cost of numerical stability
-            #     M = np.eye(d) + W * W / d  # (Yu et al. 2019)
-            #     E = np.linalg.matrix_power(M, d - 1)
-            #     h = (E.T * M).sum() - d
-            G_h = E.T * self.W * 2
+            E = torch.matrix_exp(self.W @ self.W)  # (Zheng et al. 2018)
+            h = torch.trace(E) - d
+            G_h = torch.matmul(torch.transpose(E, 0, 1), self.W) * 2
             return h, G_h
