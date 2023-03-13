ee1d84d97efcb501169311d942a9cc6d7656c0e5
Zhaoming2021 <91507587+Zhaoming2021@users.noreply.github.com>
2023-03-13 12:04:58 -0500

    modify

--- algorithms.py
+++ algorithms.py
@@ -1,58 +1,156 @@
 # augmented_lagrangize.py
 # penalty_method.py
 # quadratic_penalty.py
-
-
-import scores
-import models
 import torch
 import torch.nn as nn
 import numpy as np
-from lbfgsb_scipy import LBFGSBScipy
+from tqdm.auto import tqdm
 from  torch import optim
-# linear for notears and dagma
+import copy
+import scores
+import models
+import scipy.optimize as sopt
+import dag_function
+from lbfgsb_scipy import LBFGSBScipy
+
 class Algorithms:
-     def __init__(self, model_type, loss_type=None):
-        self.model_type = model_type
-        self.loss_type = loss_type
 
-        if model_type == "notears_linear":
+    def __init__(self, model):
+        self.model = model
 
-            self._algo = self._func_notears()
+        if self.model.type == "mlp_signed":
+            # dagma nonlinear algorithm in nonlinear
+
+            self._algo = self.dagma_nonlinear()
             
-        elif model_type == "dagma_linear":  
+        elif self.model.type == "linear_signed":
+            # dagma nonlinear algorithm in linear
 
-            self._algo = self._func_dag()
-    
-        elif model_type == "notears_mlp":
-            self._algo = self.dual_ascent_step()
+            self._algo = self.notears_linear()
 
-        elif model_type == "dagma_mlp":
-            self._algo = self.minimize()
+        elif self.model.type == "mlp_unsigned":
+            # notears nonlinear algorithm in nonlinear
+            self._algo = self.notears_nonlinear()
+
+        elif self.model.type == "linear_unsigned":
+            # notears nonlinear algorithm in linear
+            self._algo = self.notears_linear()
         
         else:
             raise ValueError("Invalid model type")
-            
-#linear for notears
-def _func_notears(w,alpha,lambda1,rho):
-        """Evaluate value and gradient of augmented Lagrangian for doubled variables ([2 d^2] array)."""
-        W = models._adj(w)
-        loss, G_loss = scores._loss(W)
-        h, G_h = models._h(W)
-        obj = loss + 0.5 * rho * h * h + alpha * h + lambda1 * w.sum()
-        G_smooth = G_loss + (rho * h + alpha) * G_h
-        g_obj = np.concatenate((G_smooth + lambda1, - G_smooth + lambda1), axis=None)
-        return obj, g_obj
+        
+def notears_linear(X, lambda_1=0., lambda_2=0., loss_type='l2', h_tol=1e-4, 
+                        w_threshold=1e-4, rho_max=1e+16, max_iter=1000):
+        """
+        Learn a linear DAG from observational data using the NOTEARS algorithm.
 
-#linear for dagma
-def _func_dag(W, mu,lambda1, s=1.0):
-        """Evaluate value of the penalized objective function."""
-        score, _ = scores._score(W)
-        h, _ = models._h(W, s)
-        obj = mu * (score + lambda1 * np.abs(W).sum()) + h 
-        return obj, score, h
+        Args:
+            X (ndarray): n x d data matrix
+            lambda_1 (float): l1 regularization parameter
+            lambda_2 (float): l2 regularization parameter
+            loss_type (str): 'l2' or 'log' for L2 loss or logistic loss, respectively
+            h_tol (float): tolerance for constraint violation, the algorithm stops if vilation is below this level
+            w_threshold (float): threshold for zeroing out small edges
+            rho_max (float): maximum value for rho, the ADMM penalty parameter
+            max_iter (int): maximum number of iterations to run the algorithm
 
-# mlp for notears
+        Returns:
+            ndarray: d x d DAG adjacency matrix
+        """
+        n, d = X.shape
+        w_est, rho, alpha, h = np.zeros(2 * d * d), 1.0, 0.0, np.inf  # double w_est into (w_pos, w_neg)
+        bnds = [(0, 0) if i == j else (0, None) for _ in range(2) for i in range(d) for j in range(d)]
+        if loss_type == 'l2':
+            X = X - np.mean(X, axis=0, keepdims=True)
+        for _ in range(max_iter):
+            w_new, h_new = None, None
+            while rho < rho_max:
+                sol = sopt.minimize(models._func, w_est, method='L-BFGS-B', jac=True, bounds=bnds)
+                w_new = sol.x
+                h_new, _ = models._h(models._adj(w_new))
+                if h_new > 0.25 * h:
+                    rho *= 10
+                else:
+                    break
+            w_est, h = w_new, h_new
+            alpha += rho * h
+            if h <= h_tol or rho >= rho_max:
+                break
+        W_est = models._adj(w_est)
+        W_est[np.abs(W_est) < w_threshold] = 0
+        return W_est
+       
+def minimize(model, X, max_iter, lr, lambda1, lambda2, mu, s, lr_decay=False, checkpoint=1000, tol=1e-6, verbose=False, pbar=None):
+    vprint = print if verbose else lambda *a, **k: None
+    vprint(f'\nMinimize s={s} -- lr={lr}')
+    optimizer = optim.Adam(model.parameters(), lr=lr, betas=(.99,.999), weight_decay=mu*lambda2)
+    if lr_decay is True:
+        scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.8)
+    obj_prev = 1e16
+    for i in range(max_iter):
+        optimizer.zero_grad()
+        h_val = model.h_func(s)
+        if h_val.item() < 0:
+            vprint(f'Found h negative {h_val.item()} at iter {i}')
+            return False
+        X_hat = model(X)
+        score = scores.log_mse_loss(X_hat, X)
+        l1_reg = lambda1 * model.fc1_l1_reg()
+        obj = mu * (score + l1_reg) + h_val
+        obj.backward()
+        optimizer.step()
+        if lr_decay and (i+1) % 1000 == 0: #every 1000 iters reduce lr
+            scheduler.step()
+        if i % checkpoint == 0 or i == max_iter-1:
+            obj_new = obj.item()
+            vprint(f"\nInner iteration {i}")
+            vprint(f'\th(W(model)): {h_val.item()}')
+            vprint(f'\tscore(model): {obj_new}')
+            if np.abs((obj_prev - obj_new) / obj_prev) <= tol:
+                pbar.update(max_iter-i)
+                break
+            obj_prev = obj_new
+        pbar.update(1)
+    return True
+       
+def dagma_nonlinear(
+        model: nn.Module, X: torch.tensor, lambda1=.02, lambda2=.005,
+        T=4, mu_init=.1, mu_factor=.1, s=1.0,
+        warm_iter=5e4, max_iter=8e4, lr=.0002, 
+        w_threshold=0.3, checkpoint=1000, verbose=False
+    ):
+        vprint = print if verbose else lambda *a, **k: None
+        mu = mu_init
+        if type(s) == list:
+            if len(s) < T: 
+                vprint(f"Length of s is {len(s)}, using last value in s for iteration t >= {len(s)}")
+                s = s + (T - len(s)) * [s[-1]]
+        elif type(s) in [int, float]:
+            s = T * [s]
+        else:
+            ValueError("s should be a list, int, or float.") 
+        with tqdm(total=(T-1)*warm_iter+max_iter) as pbar:
+            for i in range(int(T)):
+                vprint(f'\nDagma iter t={i+1} -- mu: {mu}', 30*'-')
+                success, s_cur = False, s[i]
+                inner_iter = int(max_iter) if i == T - 1 else int(warm_iter)
+                model_copy = copy.deepcopy(model)
+                lr_decay = False
+                while success is False:
+                    success = minimize(model, X, inner_iter, lr, lambda1, lambda2, mu, s_cur, 
+                                        lr_decay, checkpoint=checkpoint, verbose=verbose, pbar=pbar)
+                    if success is False:
+                        model.load_state_dict(model_copy.state_dict().copy())
+                        lr *= 0.5 
+                        lr_decay = True
+                        if lr < 1e-10:
+                            break # lr is too small
+                        s_cur = 1
+                mu *= mu_factor
+        W_est = model.fc1_to_adj()
+        W_est[np.abs(W_est) < w_threshold] = 0
+        return W_est
+
 def dual_ascent_step(model, X, lambda1, lambda2, rho, alpha, h, rho_max):
     """Perform one step of dual ascent in augmented Lagrangian."""
     h_new = None
@@ -80,36 +178,69 @@ def dual_ascent_step(model, X, lambda1, lambda2, rho, alpha, h, rho_max):
     alpha += rho * h_new
     return rho, alpha, h_new
 
-# mlp for dagma
-def minimize(model, X, max_iter, lr, lambda1, lambda2, mu, s, lr_decay=False, checkpoint=1000, tol=1e-6, verbose=False, pbar=None):
-    vprint = print if verbose else lambda *a, **k: None
-    vprint(f'\nMinimize s={s} -- lr={lr}')
-    optimizer = optim.Adam(model.parameters(), lr=lr, betas=(.99,.999), weight_decay=mu*lambda2)
-    if lr_decay is True:
-        scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.8)
-    obj_prev = 1e16
-    for i in range(max_iter):
-        optimizer.zero_grad()
-        h_val = model.h_func(s)
-        if h_val.item() < 0:
-            vprint(f'Found h negative {h_val.item()} at iter {i}')
-            return False
-        X_hat = model(X)
-        score = score.log_mse_loss(X_hat, X)
-        l1_reg = lambda1 * model.fc1_l1_reg()
-        obj = mu * (score + l1_reg) + h_val
-        obj.backward()
-        optimizer.step()
-        if lr_decay and (i+1) % 1000 == 0: #every 1000 iters reduce lr
-            scheduler.step()
-        if i % checkpoint == 0 or i == max_iter-1:
-            obj_new = obj.item()
-            vprint(f"\nInner iteration {i}")
-            vprint(f'\th(W(model)): {h_val.item()}')
-            vprint(f'\tscore(model): {obj_new}')
-            if np.abs((obj_prev - obj_new) / obj_prev) <= tol:
-                pbar.update(max_iter-i)
-                break
-            obj_prev = obj_new
-        pbar.update(1)
-    return True
+
+def notears_nonlinear(model: nn.Module,
+                      X: np.ndarray,
+                      lambda1: float = 0.,
+                      lambda2: float = 0.,
+                      max_iter: int = 100,
+                      h_tol: float = 1e-8,
+                      rho_max: float = 1e+16,
+                      w_threshold: float = 0.3):
+    rho, alpha, h = 1.0, 0.0, np.inf
+    for _ in range(max_iter):
+        rho, alpha, h = dual_ascent_step(model, X, lambda1, lambda2,
+                                         rho, alpha, h, rho_max)
+        if h <= h_tol or rho >= rho_max:
+            break
+    W_est = model.fc1_to_adj()
+    W_est[np.abs(W_est) < w_threshold] = 0
+    return W_est
+
+def fit(X, lambda1, w_threshold=0.3, T=5,
+            mu_init=1.0, mu_factor=0.1, s=[1.0, .9, .8, .7, .6], 
+            warm_iter=3e4, max_iter=6e4, lr=0.0003, 
+            checkpoint=1000, beta_1=0.99, beta_2=0.999, verbose=False, dtype=np.float64
+        ):
+        ## INITALIZING VARIABLES 
+        X, lambda1, checkpoint = X, lambda1, checkpoint
+        n, d = X.shape
+        Id = np.eye(d).astype(dtype)
+        vprint = print if verbose else lambda *a, **k: None
+        
+        if models.loss_type == 'l2':
+            X -= X.mean(axis=0, keepdims=True)
+            
+        cov = X.T @ X / float(n)    
+        W_est = np.zeros((d,d)).astype(dtype) # init W0 at zero matrix
+        mu = mu_init
+        if type(s) == list:
+            if len(s) < T: 
+                vprint(f"Length of s is {len(s)}, using last value in s for iteration t >= {len(s)}")
+                s = s + (T - len(s)) * [s[-1]]
+        elif type(s) in [int, float]:
+            s = T * [s]
+        else:
+            ValueError("s should be a list, int, or float.")    
+        
+        ## START DAGMA
+        with tqdm(total=(T-1)*warm_iter+max_iter) as pbar:
+            for i in range(int(T)):
+                vprint(f'\nIteration -- {i+1}:')
+                lr_adam, success = lr, False
+                inner_iters = int(max_iter) if i == T - 1 else int(warm_iter)
+                while success is False:
+                    W_temp, success = minimize(W_est.copy(), mu, inner_iters, s[i], lr=lr_adam, beta_1=beta_1, beta_2=beta_2, pbar=pbar)
+                    if success is False:
+                        vprint(f'Retrying with larger s')
+                        lr_adam *= 0.5
+                        s[i] += 0.1
+                W_est = W_temp
+                mu *= mu_factor
+        
+        ## Store final h and score values and threshold
+        h_final, _ = dag_function._h(W_est)  #model, X, W, dims, s=1.0
+        score_final, _ = scores._score(W_est)  #?score 能否这么写
+        W_est[np.abs(W_est) < w_threshold] = 0
+        return W_est
+    
--- demo.py
+++ demo.py
@@ -1,45 +1,38 @@
+import numpy as np
 import torch
 import torch.nn as nn
-import numpy as np
-import scipy.linalg as slin
-import scipy.linalg as sla
-import numpy.linalg as la
-import torch.nn.functional as F
-from locally_connected import LocallyConnected
-from trace_expm import trace_expm
-import dag_function
 import models
+import scores
+import dag_function
+import algorithms
+from  torch import optim
+from timeit import default_timer as timer
+import utils
 
+# nonlinear example
 
-if __name__ == '__main__':
-    from timeit import default_timer as timer
-    import utils
-    
-    torch.set_default_dtype(torch.double)
-    utils.set_random_seed(1)
-    torch.manual_seed(1)
-    
-    n, d, s0, graph_type, sem_type = 1000, 20, 20, 'ER', 'mlp'
-    B_true = utils.simulate_dag(d, s0, graph_type)
-    X = utils.simulate_nonlinear_sem(B_true, n, sem_type)
-
-    model = models.dagma_mlp(dims=[d, 10, 1], bias=True)
-    X_torch = torch.from_numpy(X)
-    tstart = timer()
-    W_est = dag_function.dagma_nonlinear(model, X_torch, lambda1=0.02, lambda2=0.005)
-    tend = timer()
-    acc = utils.count_accuracy(B_true, W_est != 0)
-    print(f'runtime: {tend-tstart}')
-    print(acc)
-
-
-
-
+torch.set_default_dtype(torch.double)
+utils.set_random_seed(1)
+torch.manual_seed(1)
+n, d, s0, graph_type, sem_type = 1000, 20, 20, 'ER', 'mlp'
+B_true = utils.simulate_dag(d, s0, graph_type)
+#W_true = utils.simulate_parameter(B_true)
+#X = utils.simulate_nonlinear_sem(W_true, n, sem_type)  
+X = utils.simulate_nonlinear_sem(B_true, n, sem_type)
 
 # in the demo.py
-model = mlp_signed(hyperparams)
-h_func = notears(model,hyperparams)
-score = mse_loss(data, model,hyperparams) 
-optimizer = adam(hyperparams) # how to solve each unconstrained problem
+model = models.mlp_signed(dims=[d, 10, 1], bias=True)
+h_func = dag_function.dag_functions(model, X, dims = [d, 10, 1], s=1.0)
+X_hat = model(X)
+loss_fn = scores.LossFunction()
+score = loss_fn.log_mse_loss(X_hat, X) 
+# optimizer = optim.adam(model.parameters(), lr=lr, betas=(.99,.999), weight_decay=mu*lambda2) # how to solve each unconstrained problem
 
-W_est  = fit.augmented_lagrangian(model, score, h_func, optimizer)
+X_torch = torch.from_numpy(X)
+W_est = algorithms.dagma_nonlinear(model,score, h_func, X_torch, lambda1=0.02, lambda2=0.005)
+#W_est  = algorithms.fit(model, score, h_func, optimizer)
+assert utils.is_dag(W_est)
+np.savetxt('W_est.csv', W_est, delimiter=',')
+acc = utils.count_accuracy(B_true, W_est != 0)
+print(acc)
+
